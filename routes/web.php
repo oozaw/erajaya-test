610679b7a0a7e245c2c1f2f6cda35d02e9cb8693
oozaw <79121990+oozaw@users.noreply.github.com>
2024-12-21 09:31:39 +0700

    Update route index

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/dashboard/data', [DashboardController::class, 'getEmployeeData'])->name('dashboard.getEmployeeData');
